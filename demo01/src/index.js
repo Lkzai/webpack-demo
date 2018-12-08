4d6cff9f5d7522f31e767194030010350ff514ed
lk <593248106@qq.com>
2018-12-08 17:16:00 +0800

    demo01 update

--- demo01/src/index.js
+++ demo01/src/index.js
@@ -1,1 +1,11 @@
-document.write('<h1>Message From Index.js</h1>');
+import _ from 'lodash';
+
+function component() {
+    var element = document.createElement('div');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' '); //要用到lodash的语法
+
+    return element;
+}
+
+document.body.appendChild(component());
